9b4424e0a8aabf214e7b832977bbfd4ec6e0f20a
Franciole Bezerra <franciole.bezerra@gmail.com>
2023-03-02 22:11:52 -0300

    Projeto criado

---
+++ src/main/java/com/franciole/cursomcnovo/CursomcnovoApplication.java
@@ -0,0 +1,13 @@
+package com.franciole.cursomcnovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcnovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcnovoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franciole/cursomcnovo/CursomcnovoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franciole.cursomcnovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcnovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
